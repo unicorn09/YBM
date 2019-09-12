65a043db068cad433f6fab57f9d558ee74f5c1ec
DELL <harshrajnit@gmail.com>
2019-09-13 00:44:00 +0530

    harsh_commit2

--- app/src/main/java/com/example/ybm/Registration.java
+++ app/src/main/java/com/example/ybm/Registration.java
@@ -1,13 +1,27 @@
 package com.example.ybm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Registration extends AppCompatActivity {
-
+public class Registration extends AppCompatActivity implements View.OnClickListener {
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        button=(Button)findViewById(R.id.get_otp);
+        button.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.get_otp)
+        {
+            startActivity(new Intent(this,Registration2.class));
+        }
     }
 }
