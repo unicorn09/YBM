483f4c9d2af6103e943a2efbfecc05c4a5629719
MeNavin <kingnavinbharti1998@gmail.com>
2019-09-13 00:44:12 +0530

    Navin seventh commit

--- app/src/main/java/com/example/ybm/BasicInfo.java
+++ app/src/main/java/com/example/ybm/BasicInfo.java
@@ -3,36 +3,40 @@ package com.example.ybm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class BasicInfo extends AppCompatActivity {
-    EditText infoname,infoage;
-    TextView male,female,other,biharbelongY,biharbelongN,currentlyY,currentlyN;
-    FirebaseAuth mAuth;
-    DatabaseReference mrefrence;
-    String name,age,gender,belongyes;
+
+    Spinner spinner;
+    TextView district;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_info);
-        infoname=findViewById(R.id.info_name);
-        infoage=findViewById(R.id.info_age);
-        male=findViewById(R.id.info_male);
-        female=findViewById(R.id.info_female);
-        other=findViewById(R.id.info_other);
-        biharbelongY=findViewById(R.id.bihar_belong_Y);
-        biharbelongN=findViewById(R.id.bihar_belong_N);
 
-        mAuth=FirebaseAuth.getInstance();
-        String s=mAuth.getCurrentUser().getUid();
-        mrefrence= FirebaseDatabase.getInstance().getReference().child("usersProfile").child(s);
+        district = findViewById(R.id.basicinfo_district);
+
+        district.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner.setVisibility(View.VISIBLE);
+                district.setVisibility(View.GONE);
+
+            }
+        });
+
+        spinner = findViewById(R.id.basicinfo_spinner);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.listOfDistricts));
+
+        spinner.setAdapter(arrayAdapter);
+
+
+
+
 
     }
-
 }
