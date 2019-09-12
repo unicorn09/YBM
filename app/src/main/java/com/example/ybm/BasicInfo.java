a0f004ae3c92f44452c4995092b03b7fe43b255b
MeNavin <kingnavinbharti1998@gmail.com>
2019-09-12 22:41:27 +0530

    Navin third commit

--- app/src/main/java/com/example/ybm/BasicInfo.java
+++ app/src/main/java/com/example/ybm/BasicInfo.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 public class BasicInfo extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_info);
     }
