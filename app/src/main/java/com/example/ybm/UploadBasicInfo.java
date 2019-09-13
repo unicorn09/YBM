573f9161e826ceab8c363022514c5ddf04b03b29
kundannitp <kundan3316@gmail.com>
2019-09-13 11:45:12 +0530

    Kundan Commit4

---
+++ app/src/main/java/com/example/ybm/UploadBasicInfo.java
@@ -0,0 +1,14 @@
+package com.example.ybm;
+
+public class UploadBasicInfo {
+    String Name,Age,Gender,BelongToBihar,District,IsCurrentlyLivingInBihar;
+
+    public UploadBasicInfo(String name, String age, String gender, String belongToBihar, String district, String isCurrentlyLivingInBihar) {
+        Name = name;
+        Age = age;
+        Gender = gender;
+        BelongToBihar = belongToBihar;
+        District = district;
+        IsCurrentlyLivingInBihar = isCurrentlyLivingInBihar;
+    }
+}
